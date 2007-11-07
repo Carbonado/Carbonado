f5b18b433e07d7a71fc2799da66b92d7248b5327
fangchen <fangchen@users.sourceforge.net>
2007-11-07 19:05:21 +0000

    Add Name annotation.

---
+++ src/main/java/com/amazon/carbonado/Name.java
@@ -0,0 +1,19 @@
+package com.amazon.carbonado;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD})
+public @interface Name {
+
+    /**
+     * The prefered name of the property
+     */
+    String value();
+
+}
