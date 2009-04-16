d5b5dc9e2082692b2d87553e4501ef2e5c8f2a64
Brian S. O'Neill <bronee@gmail.com>
2009-04-16 01:37:31 +0000

    Added properties to obtain more BDB info.

--- src/main/java/com/amazon/carbonado/repo/sleepycat/EnvironmentCapability.java
+++ src/main/java/com/amazon/carbonado/repo/sleepycat/EnvironmentCapability.java
@@ -18,6 +18,8 @@
 
 package com.amazon.carbonado.repo.sleepycat;
 
+import java.io.File;
+
 import com.amazon.carbonado.capability.Capability;
 
 /**
@@ -31,4 +33,22 @@ public interface EnvironmentCapability extends Capability {
      * type, depending on the BDB product and version being used.
      */
     Object getEnvironment();
+
+    BDBProduct getBDBProduct();
+
+    /**
+     * Returns the major, minor, and patch version numbers.
+     */
+    int[] getVersion();
+
+    /**
+     * Returns the home directory for the BDB environment.
+     */
+    File getHome();
+
+    /**
+     * Returns the directory where data files are stored, which is the same as
+     * the home directory by default.
+     */
+    File getDataHome();
 }
